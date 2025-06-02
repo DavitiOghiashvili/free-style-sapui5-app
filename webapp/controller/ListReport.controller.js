97619b296bcf32a0bd17fae88787424373420201
DavitiOghiashvili <daviti.oghiashvili@leverx.com>
2025-06-02 17:29:03 +0400

    Second page object page (#2)
    * List report, First page, first try
    
    * fixed issues
    
    * added second page
    
    * finished second page, added overall improvements
    
    * fixed remaining issues

--- webapp/controller/ListReport.controller.js
+++ webapp/controller/ListReport.controller.js
@@ -33,8 +33,8 @@ sap.ui.define([
           ],
           statuses: [
             { key: Constants.PRODUCT_STATUS.OK, text: oResourceBundle.getText("OK") },
-            { key: Constants.PRODUCT_STATUS.STORAGE, text: oResourceBundle.getText("Storage") },
-            { key: Constants.PRODUCT_STATUS.OUT_OF_STOCK, text: oResourceBundle.getText("outOfStock") }
+            { key: Constants.PRODUCT_STATUS.STORAGE, text: oResourceBundle.getText("STORAGE") },
+            { key: Constants.PRODUCT_STATUS.OUT_OF_STOCK, text: oResourceBundle.getText("OUT_OF_STOCK") }
           ],
           ratings: [
             { key: Constants.RATING_LENGTH[1], text: '1' },
@@ -44,6 +44,7 @@ sap.ui.define([
             { key: Constants.RATING_LENGTH[5], text: '5' },
           ],
           productsCount: 0,
+          searchQuery: '',
         }),
         "uiModel"
       );
@@ -56,7 +57,7 @@ sap.ui.define([
       this.oSnappedLabel = this.getView().byId("idNoFiltersActiveSnappedLabel");
       this.oFilterBar = this.getView().byId("idFilterBar");
 
-      this.oFilterBar.registerGetFiltersWithValues(this.getFiltersWithValues.bind(this));
+      this.oFilterBar.registerGetFiltersWithValues(this._getFiltersWithValues.bind(this));
     },
 
     /**
@@ -81,64 +82,11 @@ sap.ui.define([
     },
 
     /**
-     * Handle search input to filter table items.
-     * @param oEvent - Search field event
-     * @public
-     */
-    onSearchFieldSearch(oEvent) {
-      const sQuery = oEvent.getParameter("query").trim();
-      const oTable = this.byId("idProductsTable");
-      const oBinding = oTable.getBinding("items");
-      const aFilters = [];
-
-      if (sQuery) {
-        const textFields = ["Name", "Specs", "SupplierInfo", "MadeIn", "ProductionCompanyName"];
-        const textFilters = textFields.map(path =>
-          new Filter({ path, operator: FilterOperator.Contains, value1: sQuery })
-        );
-
-        if (!isNaN(sQuery)) {
-          const nQuery = Number(sQuery);
-          const maxLength = Constants.MAX_PRICE_QUERY_LENGTH;
-          const numDigitsInQuery = sQuery.length;
-          const multiplier = Math.pow(10, maxLength - numDigitsInQuery);
-
-          const value1 = nQuery * multiplier;
-          const value2 = (nQuery + 1) * multiplier - 1;
-
-          textFilters.push(
-            new Filter({
-              path: "Price_amount",
-              operator: FilterOperator.BT,
-              value1: value1,
-              value2: value2
-            })
-          );
-        }
-
-        if (!isNaN(sQuery)) {
-          textFilters.push(
-            new Filter({
-              path: "Rating",
-              operator: FilterOperator.EQ,
-              value1: Number(sQuery)
-            })
-          );
-        }
-
-        aFilters.push(new Filter({ filters: textFilters, and: false }));
-      }
-
-      oBinding.filter(aFilters);
-      this._updateFilteredCount(aFilters);
-    },
-
-    /**
      * Retrieve filter group items with values.
      * @returns {Array} Array of filter group items with selected values
-     * @public
+     * @private
      */
-    getFiltersWithValues() {
+    _getFiltersWithValues() {
       const aFiltersWithValue = this.oFilterBar.getFilterGroupItems().reduce((aResult, oFilterGroupItem) => {
         const oControl = oFilterGroupItem.getControl();
         if (oControl.getSelectedKeys().length > 0) {
@@ -164,15 +112,66 @@ sap.ui.define([
      */
     onFilterBarSearch() {
       const oTable = this.getView().byId("idProductsTable");
-      const aTableFilters = [];
+      const oBinding = oTable.getBinding("items");
+      const aFilters = [];
+
+      const sQuery = this.getView().getModel("uiModel").getProperty("/searchQuery")
+      const textFields = Constants.SEARCH_FILTERS.byText
+
+      if (sQuery) {
+        const aTextFieldFilters = textFields.map((field) => {
+          return new Filter({
+            path: field,
+            operator: FilterOperator.Contains,
+            caseSensitive: false,
+            value1: sQuery
+          });
+        });
+
+        if (!isNaN(sQuery)) {
+          const nQuery = Number(sQuery);
+          const maxLength = Constants.MAX_PRICE_QUERY_LENGTH;
+          const numDigitsInQuery = sQuery.length;
+          const multiplier = Math.pow(10, maxLength - numDigitsInQuery);
+
+          const value1 = nQuery * multiplier;
+          const value2 = (nQuery + 1) * multiplier - 1;
+
+          aFilters.push(
+            new Filter({
+              path: "Price_amount",
+              operator: FilterOperator.BT,
+              value1: value1,
+              value2: value2
+            })
+          );
+        }
+
+        if (!isNaN(sQuery)) {
+          aFilters.push(
+            new Filter({
+              path: "Rating",
+              operator: FilterOperator.EQ,
+              value1: Number(sQuery)
+            })
+          );
+        }
+
+        aFilters.push(new Filter({
+          filters: aTextFieldFilters,
+          and: false
+        }));
+      }
 
       this.oFilterBar.getFilterGroupItems().forEach((oFilterGroupItem) => {
+        const sFieldName = oFilterGroupItem.getName();
+        if (sFieldName === 'Search Field') return;
+
         const oControl = oFilterGroupItem.getControl();
         const aSelectedKeys = oControl.getSelectedKeys();
-        const sFieldName = oFilterGroupItem.getName();
 
         if (aSelectedKeys.length > 0) {
-          const aFilters = aSelectedKeys.map((sSelectedKey) => {
+          const aFieldFilters = aSelectedKeys.map((sSelectedKey) => {
             return new Filter({
               path: sFieldName,
               operator: sFieldName === "Rating" ? FilterOperator.EQ : FilterOperator.Contains,
@@ -180,16 +179,16 @@ sap.ui.define([
             });
           });
 
-          aTableFilters.push(new Filter({
-            filters: aFilters,
+          aFilters.push(new Filter({
+            filters: aFieldFilters,
             and: false
           }));
         }
+
       });
 
-      oTable.getBinding("items").filter(aTableFilters);
-      oTable.setShowOverlay(false);
-      this._updateFilteredCount(aTableFilters);
+      oBinding.filter(aFilters);
+      this._updateFilteredCount(aFilters);
     },
 
     /**
@@ -215,6 +214,20 @@ sap.ui.define([
     _updateLabelsAndTable() {
       this.oExpandedLabel.setText(Formatter.getFormattedSummaryTextExpanded(this.oFilterBar));
       this.oSnappedLabel.setText(Formatter.getFormattedSummaryText(this.oFilterBar));
+    },
+
+    /**
+     * Navigate to item on press.
+     * @param oEvent item press event.
+     * @public
+     */
+    onColumnListItemPress(oEvent) {
+      const oContext = oEvent.getSource().getBindingContext();
+      const sProductId = oContext.getProperty("ID");
+
+      this.getOwnerComponent().getRouter().navTo("ObjectPage", {
+        Product_ID: sProductId,
+      });
     }
   });
 });
--- webapp/utils/Formatter.js
+++ webapp/utils/Formatter.js
@@ -1,13 +1,45 @@
 sap.ui.define([
-    "sap/ui/model/resource/ResourceModel"
-], (ResourceModel) => {
+    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/core/library",
+    "freestylesapui5app/utils/Constants"
+], (ResourceModel,
+    coreLibrary,
+    Constants) => {
     "use strict";
-
     const oResourceBundle = new ResourceModel({
         bundleName: "freestylesapui5app.i18n.i18n"
     }).getResourceBundle();
 
+    const { ValueState } = coreLibrary
+
     return {
+        /**
+         * Format the status
+         * @param {string} statusValue - The product status
+         * @returns {sap.ui.core.library.ValueState} ValueState - Formatted status state
+         */
+        productStatusState(statusValue) {
+            switch (statusValue) {
+                case Constants.PRODUCT_STATUS.OK:
+                    return ValueState.Success;
+                case Constants.PRODUCT_STATUS.STORAGE:
+                    return ValueState.Warning;
+                case Constants.PRODUCT_STATUS.OUT_OF_STOCK:
+                    return ValueState.Error;
+                default:
+                    return ValueState.None;
+            }
+        },
+
+        /**
+         * Format the status text
+         * @param {string} statusValue - The product status
+         * @returns {string} Formatted title - The corresponding text for the product status
+         */
+        productStatusText(sStatusValue) {
+            return oResourceBundle.getText(sStatusValue);
+        },
+
         /**
          * Format the product count title
          * @param {number} iCount - The product count
