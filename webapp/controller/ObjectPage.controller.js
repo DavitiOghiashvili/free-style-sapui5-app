a34d3a0bf5c2df8c81c64b87f80e1eca4fab796d
Daviti Oghiashvili <daviti.oghiashvili@leverx.com>
2025-06-05 15:44:59 +0400

    added product deletion on OP

--- webapp/controller/ObjectPage.controller.js
+++ webapp/controller/ObjectPage.controller.js
@@ -1,17 +1,32 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "freestylesapui5app/utils/Formatter"
+  "freestylesapui5app/utils/Formatter",
+  "sap/m/MessageBox",
+  "sap/m/MessageToast",
+  "sap/ui/core/BusyIndicator",
 ], function (Controller,
-  Formatter) {
+  Formatter,
+  MessageBox,
+  MessageToast,
+  BusyIndicator,) {
   "use strict";
   return Controller.extend("freestylesapui5app.controller.ObjectPage", {
     formatter: Formatter,
 
     onInit() {
+      this._oResourceBundle = this.getOwnerComponent()
+        .getModel("i18n")
+        .getResourceBundle();
+
       this.getOwnerComponent()
         .getRouter()
         .getRoute("ObjectPage")
         .attachPatternMatched(this._onRouteMatched, this);
+
+      const oViewModel = new sap.ui.model.json.JSONModel({
+        editMode: false
+      });
+      this.getView().setModel(oViewModel, "viewModel");
     },
 
     /**
@@ -40,6 +55,42 @@ sap.ui.define([
       this.getOwnerComponent()
         .getRouter()
         .navTo("ObjectChartPage")
-    }
+    },
+
+    /**
+       * Product delete button handler.
+       * @public
+       */
+    onDeleteButtonPress() {
+      const oMainModel = this.getView().getModel();
+      const oCtx = this.getView().getBindingContext();
+      const sKey = oMainModel.createKey("/Products", oCtx.getObject());
+
+      MessageBox.confirm(this._oResourceBundle.getText("confirmDeleteProduct"), {
+        onClose: (sAction) => {
+          if (sAction === MessageBox.Action.OK) {
+            BusyIndicator.show();
+            oMainModel.remove(sKey, {
+              success: () => {
+                BusyIndicator.hide();
+                MessageToast.show(
+                  this._oResourceBundle.getText("productDeleteSuccess"),
+                  {
+                    closeOnBrowserNavigation: false,
+                  }
+                );
+                this.getOwnerComponent().getRouter().navTo("ListReport");
+              },
+              error: (oError) => {
+                BusyIndicator.hide();
+                MessageBox.error(this._oResourceBundle.getText("productDeleteError"), {
+                  details: oError,
+                });
+              },
+            });
+          }
+        },
+      });
+    },
   });
 });
