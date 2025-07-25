53c69519196b5245ec243ba0324421c97175159b
Daviti Oghiashvili <daviti.oghiashvili@leverx.com>
2025-07-25 11:46:09 +0400

    added pure function unit test

--- webapp/test/unit/controller/ListReport.controller.js
+++ webapp/test/unit/controller/ListReport.controller.js
@@ -1,16 +1,56 @@
-/*global QUnit*/
-
 sap.ui.define(
-  ['freestylesapui5app/controller/ListReport.controller'],
-  function (Controller) {
+  ['freestylesapui5app/utils/Formatter',
+    'freestylesapui5app/utils/Constants',
+    'sap/ui/core/library',
+  ],
+  function (
+    Formatter,
+    Constants,
+    coreLibrary
+  ) {
     'use strict';
+    const { ValueState } = coreLibrary;
 
-    QUnit.module('ListReport Controller');
+    QUnit.module("Status State and color", () => {
 
-    QUnit.test('I should test the ListReport controller', function (assert) {
-      var oAppController = new Controller();
-      oAppController.onInit();
-      assert.ok(oAppController);
+      const statusStateTestCase = (oOptions) => {
+        const sState = Formatter.productStatusState(oOptions.statusValue)
+
+        oOptions.assert.strictEqual(sState, oOptions.expected, 'The status was assigned correctly')
+      }
+
+      QUnit.test('Should format OK status as Success color', (assert) => {
+        statusStateTestCase.call(this, {
+          assert: assert,
+          statusValue: Constants.PRODUCT_STATUS.OK,
+          expected: ValueState.Success
+        })
+      })
+
+      QUnit.test('Should format STORAGE status as Warning color', (assert) => {
+        statusStateTestCase.call(this, {
+          assert: assert,
+          statusValue: Constants.PRODUCT_STATUS.STORAGE,
+          expected: ValueState.Warning
+        })
+      })
+
+      QUnit.test('Should format OUT_OF_STOCK status as Error color', (assert) => {
+        statusStateTestCase.call(this, {
+          assert: assert,
+          statusValue: Constants.PRODUCT_STATUS.OUT_OF_STOCK,
+          expected: ValueState.Error
+        })
+      })
+
+      QUnit.test('Should format everything that isn"t OK, STORAGE or OUT_OF_STOCK status as None color', (assert) => {
+        statusStateTestCase.call(this, {
+          assert: assert,
+          statusValue: '',
+          expected: ValueState.None
+        })
+      })
+
+
     });
-  },
-);
+  });
