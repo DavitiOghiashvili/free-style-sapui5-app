57acb03b3df8ec0e2e130990ac2af3368e384737
Daviti Oghiashvili <daviti.oghiashvili@leverx.com>
2025-07-31 11:41:04 +0400

    added list report journey product creation tests

--- webapp/test/integration/ListReportJourney.js
+++ webapp/test/integration/ListReportJourney.js
@@ -19,13 +19,128 @@ sap.ui.define([
 
     opaTest("Should see the ListReport Products Table", function (Given, When, Then) {
         Then.onTheListReportPage.iShouldSeeTheProductsTable();
+    });
 
+    opaTest("Should open and close Product create dialog", function (Given, When, Then) {
         When.onTheListReportPage.iClickOnTheCreateButton()
+
+        Then.onTheListReportPage.iShouldSeeTheProductCreateDialog();
+
         When.onTheListReportPage.iClickOnTheSelectStoreButton()
         When.onTheListReportPage.iClickOnTheSelectStoreFirstItem()
         When.onTheListReportPage.iClickOnTheCancelProductCreateDialogButton()
-
-        Then.iTeardownMyApp()
     });
+
+    opaTest("Should create product successfully", function (Given, When, Then) {
+        When.onTheListReportPage.iClickOnTheCreateButton()
+
+        Then.onTheListReportPage.iShouldSeeTheProductCreateDialog();
+        When.onTheListReportPage.iClickOnTheSelectStoreButton()
+        When.onTheListReportPage.iClickOnTheSelectStoreFirstItem()
+        When.onTheListReportPage.iEnterProductName();
+        When.onTheListReportPage.iEnterProductPrice();
+        When.onTheListReportPage.iEnterProductSpecs();
+        When.onTheListReportPage.iEnterProductRating();
+        When.onTheListReportPage.iEnterProductSupplierInfo();
+        When.onTheListReportPage.iEnterProductMadeIn();
+        When.onTheListReportPage.iEnterProductProdCompany();
+
+        When.onTheListReportPage.iClickOnTheOpenProductStatusSelect();
+        When.onTheListReportPage.iClickOnTheStatusSelectItem()
+
+        When.onTheListReportPage.iClickOnTheConfirmProductCreateButton();
+        Then.iTeardownMyApp();
+    });
+
+    opaTest("Should validate fields before creating product", function (Given, When, Then) {
+        Given.iStartMyApp();
+        When.onTheListReportPage.iClickOnTheCreateButton()
+
+        Then.onTheListReportPage.iShouldSeeTheProductCreateDialog();
+        When.onTheListReportPage.iClickOnTheSelectStoreButton()
+        When.onTheListReportPage.iClickOnTheSelectStoreFirstItem()
+
+        When.onTheListReportPage.iEnterProductSpecs();
+        When.onTheListReportPage.iEnterProductRating();
+        When.onTheListReportPage.iEnterProductSupplierInfo();
+        When.onTheListReportPage.iEnterProductMadeIn();
+        When.onTheListReportPage.iEnterProductProdCompany();
+
+        When.onTheListReportPage.iClickOnTheOpenProductStatusSelect();
+        When.onTheListReportPage.iClickOnTheStatusSelectItem()
+
+        When.onTheListReportPage.iClickOnTheConfirmProductCreateButton();
+        Then.iTeardownMyApp();
+    });
+
+    opaTest("Should appear confirmation before closing create dialog with inputted info", function (Given, When, Then) {
+        Given.iStartMyApp();
+        When.onTheListReportPage.iClickOnTheCreateButton()
+
+        Then.onTheListReportPage.iShouldSeeTheProductCreateDialog();
+        When.onTheListReportPage.iClickOnTheSelectStoreButton()
+        When.onTheListReportPage.iClickOnTheSelectStoreFirstItem()
+        When.onTheListReportPage.iEnterProductName();
+        When.onTheListReportPage.iEnterProductPrice();
+        When.onTheListReportPage.iEnterProductSpecs();
+        When.onTheListReportPage.iEnterProductRating();
+        When.onTheListReportPage.iEnterProductSupplierInfo();
+        When.onTheListReportPage.iEnterProductMadeIn();
+        When.onTheListReportPage.iEnterProductProdCompany();
+
+        When.onTheListReportPage.iClickOnTheOpenProductStatusSelect();
+        When.onTheListReportPage.iClickOnTheStatusSelectItem()
+
+        When.onTheListReportPage.iClickOnTheCancelProductCreateDialogButton();
+        Then.iTeardownMyApp();
+    });
+
+    opaTest("Should confirm closing product create dialog", function (Given, When, Then) {
+        Given.iStartMyApp();
+        When.onTheListReportPage.iClickOnTheCreateButton()
+
+        Then.onTheListReportPage.iShouldSeeTheProductCreateDialog();
+        When.onTheListReportPage.iClickOnTheSelectStoreButton()
+        When.onTheListReportPage.iClickOnTheSelectStoreFirstItem()
+        When.onTheListReportPage.iEnterProductName();
+        When.onTheListReportPage.iEnterProductPrice();
+        When.onTheListReportPage.iEnterProductSpecs();
+        When.onTheListReportPage.iEnterProductRating();
+        When.onTheListReportPage.iEnterProductSupplierInfo();
+        When.onTheListReportPage.iEnterProductMadeIn();
+        When.onTheListReportPage.iEnterProductProdCompany();
+
+        When.onTheListReportPage.iClickOnTheOpenProductStatusSelect();
+        When.onTheListReportPage.iClickOnTheStatusSelectItem()
+
+        When.onTheListReportPage.iClickOnTheCancelProductCreateDialogButton();
+        When.onTheListReportPage.iConfirmTheMessageBox();
+        Then.iTeardownMyApp();
+    });
+
+    opaTest("Should close product create dialog", function (Given, When, Then) {
+        Given.iStartMyApp();
+        When.onTheListReportPage.iClickOnTheCreateButton()
+
+        Then.onTheListReportPage.iShouldSeeTheProductCreateDialog();
+        When.onTheListReportPage.iClickOnTheSelectStoreButton()
+        When.onTheListReportPage.iClickOnTheSelectStoreFirstItem()
+        When.onTheListReportPage.iEnterProductName();
+        When.onTheListReportPage.iEnterProductPrice();
+        When.onTheListReportPage.iEnterProductSpecs();
+        When.onTheListReportPage.iEnterProductRating();
+        When.onTheListReportPage.iEnterProductSupplierInfo();
+        When.onTheListReportPage.iEnterProductMadeIn();
+        When.onTheListReportPage.iEnterProductProdCompany();
+
+        When.onTheListReportPage.iClickOnTheOpenProductStatusSelect();
+        When.onTheListReportPage.iClickOnTheStatusSelectItem()
+
+        When.onTheListReportPage.iClickOnTheCancelProductCreateDialogButton();
+        When.onTheListReportPage.iCancelTheMessageBox();
+        Then.iTeardownMyApp();
+    });
+
+
 
 });
--- webapp/test/integration/pages/ListReport.js
+++ webapp/test/integration/pages/ListReport.js
@@ -1,10 +1,22 @@
-sap.ui.define(['sap/ui/test/Opa5', 'sap/ui/test/actions/Press'], function (Opa5, Press) {
+sap.ui.define([
+  'sap/ui/test/Opa5',
+  'sap/ui/test/actions/Press',
+  'sap/ui/test/actions/EnterText',
+  'sap/ui/test/matchers/PropertyStrictEquals'
+], function (
+  Opa5,
+  Press,
+  EnterText,
+  PropertyStrictEquals
+) {
   'use strict';
+
   const sViewName = 'ListReport';
 
   Opa5.createPageObjects({
     onTheListReportPage: {
       actions: {
+
         iClickOnTheFirstItem: function () {
           return this.waitFor({
             controlType: "sap.m.ColumnListItem",
@@ -43,6 +55,118 @@ sap.ui.define(['sap/ui/test/Opa5', 'sap/ui/test/actions/Press'], function (Opa5,
           });
         },
 
+        iEnterProductName: function () {
+          return this.waitFor({
+            id: "idNameInput",
+            viewName: sViewName,
+            actions: new EnterText({ text: "Axali" }),
+            success: function () {
+              Opa5.assert.ok(true, "Entered product name: " + "Axali");
+            },
+            errorMessage: "Could not find Product Name input field"
+          });
+        },
+
+        iEnterProductPrice: function () {
+          return this.waitFor({
+            id: "idPriceAmountInput",
+            viewName: sViewName,
+            actions: new EnterText({ text: "123" }),
+            success: function () {
+              Opa5.assert.ok(true, "Entered product name: " + "123");
+            },
+            errorMessage: "Could not find Product Name input field"
+          });
+        },
+
+        iEnterProductSpecs: function () {
+          return this.waitFor({
+            id: "idSpecsInput",
+            viewName: sViewName,
+            actions: new EnterText({ text: "Specsi" }),
+            success: function () {
+              Opa5.assert.ok(true, "Entered product name: " + "Specsi");
+            },
+            errorMessage: "Could not find Product Name input field"
+          });
+        },
+
+        iEnterProductRating: function () {
+          return this.waitFor({
+            id: "idRatingStepInput",
+            viewName: sViewName,
+            actions: new EnterText({ text: "3" }),
+            success: function () {
+              Opa5.assert.ok(true, "Entered product name: " + "3");
+            },
+            errorMessage: "Could not find Product Name input field"
+          });
+        },
+
+        iEnterProductSupplierInfo: function () {
+          return this.waitFor({
+            id: "idSupplierInfoInput",
+            viewName: sViewName,
+            actions: new EnterText({ text: "Axlis info" }),
+            success: function () {
+              Opa5.assert.ok(true, "Entered product name: " + "Axlis info");
+            },
+            errorMessage: "Could not find Product Name input field"
+          });
+        },
+
+        iEnterProductMadeIn: function () {
+          return this.waitFor({
+            id: "idMadeInInput",
+            viewName: sViewName,
+            actions: new EnterText({ text: "Made inis info" }),
+            success: function () {
+              Opa5.assert.ok(true, "Entered product name: " + "Made inis info");
+            },
+            errorMessage: "Could not find Product Name input field"
+          });
+        },
+
+        iEnterProductProdCompany: function () {
+          return this.waitFor({
+            id: "idProductionCompanyNameInput",
+            viewName: sViewName,
+            actions: new EnterText({ text: "Qartuli compania" }),
+            success: function () {
+              Opa5.assert.ok(true, "Entered product name: " + "Qartuli compania");
+            },
+            errorMessage: "Could not find Product Name input field"
+          });
+        },
+
+        iClickOnTheOpenProductStatusSelect: function () {
+          return this.waitFor({
+            id: "idSelectStatus",
+            viewName: sViewName,
+            actions: new Press(),
+            errorMessage: "Could not press the Product Status select"
+          });
+        },
+
+        iClickOnTheStatusSelectItem: function () {
+          return this.waitFor({
+            controlType: "sap.ui.core.Item",
+            success: function (aItems) {
+              aItems[2].$().trigger("tap");
+            },
+            errorMessage: "Could not find any status list items in the dropdown"
+          });
+        },
+
+        iClickOnTheConfirmProductCreateButton: function () {
+          return this.waitFor({
+            id: "idConfirmProductCreateButton",
+            viewName: sViewName,
+            actions: new Press(),
+            errorMessage: "Could not find the Create button"
+          });
+        },
+
         iClickOnTheCancelProductCreateDialogButton: function () {
           return this.waitFor({
             id: "idCancelProductCreateDialogButton",
@@ -51,9 +175,45 @@ sap.ui.define(['sap/ui/test/Opa5', 'sap/ui/test/actions/Press'], function (Opa5,
             errorMessage: "Could not find the Cancel button"
           });
         },
+
+        iConfirmTheMessageBox: function () {
+          return this.waitFor({
+            controlType: "sap.m.Button",
+            matchers: [
+              new PropertyStrictEquals({
+                name: "text",
+                value: "OK"
+              }),
+            ],
+            actions: new Press(),
+            success: function () {
+              Opa5.assert.ok(true, "Pressed OK on the MessageBox");
+            },
+            errorMessage: "Could not find or press the OK button on the MessageBox"
+          });
+        },
+
+        iCancelTheMessageBox: function () {
+          return this.waitFor({
+            controlType: "sap.m.Button",
+            matchers: [
+              new PropertyStrictEquals({
+                name: "text",
+                value: "Cancel"
+              }),
+            ],
+            actions: new Press(),
+            success: function () {
+              Opa5.assert.ok(true, "Pressed Cancel on the MessageBox");
+            },
+            errorMessage: "Could not find or press the Cancel button on the MessageBox"
+          });
+        },
+
       },
 
       assertions: {
+
         iShouldSeeThePageListReport: function () {
           return this.waitFor({
             id: 'idListReportPage',
@@ -97,6 +257,18 @@ sap.ui.define(['sap/ui/test/Opa5', 'sap/ui/test/actions/Press'], function (Opa5,
             errorMessage: 'Products Table not found',
           });
         },
+
+        iShouldSeeTheProductCreateDialog: function () {
+          return this.waitFor({
+            id: 'idCreateProductDialog',
+            viewName: sViewName,
+            success: function () {
+              Opa5.assert.ok(true, "Product Create Dialog is visible");
+            },
+            errorMessage: "Product Create Dialog not found"
+          });
+        }
+        
       },
     },
   });
